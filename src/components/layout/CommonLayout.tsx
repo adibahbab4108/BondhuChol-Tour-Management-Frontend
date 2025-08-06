d2f6e60ef2001e6476c6dcf16d86b4828e46562c
adibahbab4108 <adibalvi476@gmail.com>
2025-08-06 23:07:34 +0600

    Folder structure with basic layout initialized

---
+++ src/components/layout/CommonLayout.tsx
@@ -0,0 +1,16 @@
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+export default function CommonLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen flex flex-col ">
+      <Navbar />
+      <div className="grow-1">{children}</div>
+      <Footer />
+    </div>
+  );
+}
---
+++ src/pages/About.tsx
@@ -0,0 +1,16 @@
+
+export default function About() {
+  return (
+    <>
+    <div className="container mx-auto px-4 py-16">
+      <h1>This is About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+        perspiciatis unde ducimus commodi eos maiores quisquam omnis ipsa
+        reiciendis reprehenderit? Mollitia nobis quod nesciunt praesentium
+        commodi quam laudantium neque eaque?
+      </p>
+    </div>
+    </>
+  );
+}
